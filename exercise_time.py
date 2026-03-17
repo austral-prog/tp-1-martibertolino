0b6abeeb1e02f9fca22833279ec1a12783df4f6a
martibertolino <virginiacesarini195@gmail.com>
2026-03-17 13:34:37 -0300

    Update exercise_time.py

--- exercise_time.py
+++ exercise_time.py
@@ -1,12 +1,17 @@
-def temperature():
+def time():
     """
-    Ejercicio 3 - Conversión de Temperatura
+    Ejercicio 4 - Calculadora de Tiempo
 
-    Dada una temperatura en Celsius, imprimir:
-    1. La temperatura en Fahrenheit (F = C × 9/5 + 32)
-    2. La temperatura original en Celsius
+    Dado un total de segundos, calcular e imprimir:
+    1. Horas completas
+    2. Minutos completos restantes
+    3. Segundos restantes
     """
-    celsius = 25
-    tempfahrenheit=(celsius*9/5+32)
-    print(tempfahrenheit)
-    print(celsius)
+    total_segundos = 3665
+
+    horacompletas=(total_segundos//3600)
+    print(horacompletas)
+    minutoscomp=((total_segundos%3600)//60)
+    print(minutoscomp)
+    segundosrest=(((total_segundos%3600)%6))
+    print(segundosrest)
